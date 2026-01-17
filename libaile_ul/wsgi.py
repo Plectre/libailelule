cf36232f0d90b7ae38798909536489116667926f
Plectre <Plectre@gmail.com>
2026-01-17 07:46:46 +0100

    first commit

---
+++ libaile_ul/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for libaile_ul project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'libaile_ul.settings')
+
+application = get_wsgi_application()

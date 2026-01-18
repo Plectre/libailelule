b9df9bbff193a5c3fb61c11253a4775ee42d1ecd
Plectre <Plectre@gmail.com>
2026-01-18 20:12:23 +0100

    add template

--- page/models.py
+++ page/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Articles(models.Model):
+    title = models.CharField(max_length=40)
+    content = models.TextField()
+
+    def __str__(self) -> str:
+        return self.title
--- page/views.py
+++ page/views.py
@@ -1,9 +1,16 @@
+
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Articles as article
 
 def index(request):
-    return render(request, 'index.html')
+    
+    context = {}
+    instance = article.objects.all()
+    return render(request, 'page/index.html', {"context" : instance})
 
 
 def club(request):
-    return render(request, "club.html")
+    return render(request, "page/club.html", context={"club":"club",})
+
+def base(request):
+    return render(request, "page/base.html")
